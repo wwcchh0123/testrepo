8bf1260a098e2b2dd2719c7e37d9f1027dace0f0
wwcchh0123 <1170142389@qq.com>
2024-12-09 16:20:58 +0800

    test14

--- testfile/test.go
+++ testfile/test.go
@@ -1,10 +1,13 @@
 package test
 
+import "fmt"
+
 func test() {
-		//testerr 111111
+	//testerr 111111
+	var test string
+	fmt.Println("test")
 }
 
-
-func test2(){
-			//testerr 33333
+func test2() {
+	//testerr 33333
 }
